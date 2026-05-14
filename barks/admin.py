e98f19b1b1ec629bc81f5f225757867d0ad6b32d
Borja <borja.bajo.fdez@gmail.com>
2026-05-14 12:06:27 +0200

    Modelo de datos: implementación de modelo en models.py, "migrations" para crear base de datos en SQLite, creación de usuario admin, creación de panel de administración de base de datos

--- barks/admin.py
+++ barks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Bark
 
-# Register your models here.
+# Register Bark model in the admin
+@admin.register(Bark) # decorator to avoid implement "admin.site.register(Bark, BarkAdmin)"
+class BarkAdmin(admin.ModelAdmin):
+    list_display = ('user', 'content', 'created_at') # to display these columns
+
+
+
--- barks/models.py
+++ barks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+# auth_user model import. It already exists since it's Djando native
+from  django.contrib.auth.models import User
 
-# Create your models here.
+# Create "barks" table model
+class Bark(models.Model):
+    # id column is created by default
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # Called "user" because Django rename it as user_id after
+    content = models.CharField(max_length=140) # CharField to add 140 limit constrain
+    created_at = models.DateTimeField(auto_now_add=True) # To set the first time bark creation only
+
+    # method to show the content on the admin panel.
+    # It takes the username from user and limit the content to 20 chars
+    def __str__(self):
+        return f"{self.user.username}: {self.content[:20]}"
